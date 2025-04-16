fd28f600009eae10b7489636d7e5db42f2844aa7
Matthew Linkous <matthew.linkous@gmail.com>
2025-04-16 14:15:34 -0500

    Initial data for Triplit 1.0

--- data.js
+++ data.js
@@ -2,40 +2,73 @@
 import { LandscapeSchema } from '@localfirstfm/landscape-schema'
 
 export const data = LandscapeSchema.make({
-	Version: 1,
-	Id: 'supersync',
-	Name: 'SuperSync',
-	Description: 'A cool way to sync data',
-	Website: 'https://supersync.cool',
-	Deployment: ['Self-hosted'],
-	License: 'MIT',
-	AppTarget: {
-		LanguageSDK: { data: ['typescript'] },
-	},
-	Networking: {
-		Topology: { data: 'Client-Server' },
-	},
-	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
-	},
-	ClientSideData: {
-		QueryAPI: { data: ['Async'] },
-		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
-		PersistenceFeatures: { data: 'Indexes' },
-		DataModel: { data: 'Document' },
-		OfflineReads: { data: 'Full Support' },
-		OfflineWrites: { data: 'Local conflict resolution' },
-		DataSize: { data: 'Up to 5-10 MB per document' },
-	},
-	SynchronizationStrategy: {
-		FullOrPartialReplication: { data: ['Full Replication'] },
-		ConflictHandling: { data: 'Automatic via CRDT' },
-		WhereResolutionOccurs: { data: 'Client' },
-		WhatGetsSynced: {
-			data: {
-				ClientToClient: 'Ops',
-			},
-		},
-	},
+  Version: 1,
+  Id: 'triplit',
+  Name: 'Triplit',
+  Description: "The Typescript-first database that handles caching and syncing for your app automatically",
+  Website: 'https://triplit.dev',
+  Deployment: ['Self-hosted', 'Hosted'],
+  License: 'GPL3',
+  AppTarget: {
+    Platform: {
+      data: ['Browser', 'Node']
+    },
+    LanguageSDK: { data: ['typescript'] },
+    FrameworkIntegrations: {
+      data: ['React', 'Vue', 'Svelte', 'Angular', 'React Native']
+    }
+  },
+  Networking: {
+    Protocol: {
+      data: ['WebSockets', 'HTTP']
+    },
+    Topology: { data: 'Client-Server' },
+  },
+  ServerSideData: {
+    PersistenceMechanism: { data: ['SQLite', 'Cloudflare Durable Object Storage', 'Custom'], comment: 'Adapter for k-v stores.' },
+    DataModelParadigm: { data: 'Relational' },
+  },
+  ClientSideData: {
+    QueryAPI: { data: ['Async', 'Signals-based Reactivity'] },
+    DataModel: { data: 'Relational' },
+    PersistenceMechanism: { data: ['IndexedDB', 'SQLite', 'Custom'], 'comment': 'Adapter for k-v stores.' },
+    PersistenceFeatures: { data: 'Indexes' },
+    SchemaManagement: {
+      data: ['Derived types']
+    },
+    OfflineReads: {
+      data: 'Query Cache',
+      comment: 'Subscribed queries have full db support including novel queries'
+    },
+    OfflineWrites: {
+      data: 'Cached offline writes',
+    },
+    DataSize: {
+      data: 'limited by device capabilities'
+    },
+  },
+  SynchronizationStrategy: {
+    Authority: {
+      data: 'Centralized'
+    },
+    FullOrPartialReplication: { data: ['Partial Replication'] },
+    ConflictHandling: {
+      data: 'LWW at the attribute level'
+    },
+    WhereResolutionOccurs: { data: 'Server' },
+    WhatGetsSynced: {
+      data: {
+        ClientToServer: 'Only changes',
+        ServerToClient: 'Only changes',
+      },
+    },
+  },
+  AuthIdentity: {
+    AuthenticationMethod: {
+      data: ['JWT Tokens']
+    },
+    AuthorizationPermissions: {
+      data: 'Custom mapping from token attributes to query filters'
+    }
+  }
 })
