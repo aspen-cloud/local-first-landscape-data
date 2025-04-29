026ddb8fef8ee1480dbf98a348ecb1e3dacb1398
Matthew Linkous <matthew.linkous@gmail.com>
2025-04-28 22:07:26 -0500

    Update details

--- data.js
+++ data.js
@@ -9,14 +9,17 @@ export const data = LandscapeSchema.make({
   Website: 'https://triplit.dev',
   Deployment: ['Self-hosted', 'Hosted'],
   License: 'GPL3',
+  InitialReleaseDate: new Date('2023-07-27'),
+  MaturityLevel: 'Production-Ready',
   AppTarget: {
     Platform: {
-      data: ['Browser', 'Node']
+      data: ['Browser', 'Node', 'Deno', 'Bun', 'Cloudflare Workers', 'Hermes']
     },
-    LanguageSDK: { data: ['typescript'] },
+    LanguageSDK: { data: ['Typescript'] },
     FrameworkIntegrations: {
       data: ['React', 'Vue', 'Svelte', 'Angular', 'React Native']
-    }
+    },
+    ClientBundleSize: { data: '~70kB (gzip) ' },
   },
   Networking: {
     Protocol: {
@@ -25,20 +28,30 @@ export const data = LandscapeSchema.make({
     Topology: { data: 'Client-Server' },
   },
   ServerSideData: {
-    PersistenceMechanism: { data: ['SQLite', 'Cloudflare Durable Object Storage', 'Custom'], comment: 'Adapter for k-v stores.' },
+    PersistenceMechanism: { data: ['SQLite', 'Cloudflare Durable Object Storage', 'Custom'], comment: 'Pluggable with simple Key-Value interface' },
     DataModelParadigm: { data: 'Relational' },
+    SchemaManagement: {
+      data: ['Derived types', 'Schema migrations', 'Validate schemas on write'],
+
+    }
   },
   ClientSideData: {
-    QueryAPI: { data: ['Async', 'Signals-based Reactivity'] },
+    QueryAPI: { data: ['Async', 'Reactive queries'] },
+    LocalRefreshLatency: {
+      data: '~1ms',
+    },
     DataModel: { data: 'Relational' },
     PersistenceMechanism: { data: ['IndexedDB', 'SQLite', 'Custom'], 'comment': 'Adapter for k-v stores.' },
-    PersistenceFeatures: { data: 'Indexes' },
+    PersistenceFeatures: { data: 'Transactions' },
     SchemaManagement: {
       data: ['Derived types']
     },
     OfflineReads: {
       data: 'Query Cache',
       comment: 'Subscribed queries have full db support including novel queries'
+    },
+    'OptimisticUpdates': {
+      data: 'Yes'
     },
     OfflineWrites: {
       data: 'Cached offline writes',
@@ -58,8 +71,8 @@ export const data = LandscapeSchema.make({
     WhereResolutionOccurs: { data: 'Server' },
     WhatGetsSynced: {
       data: {
-        ClientToServer: 'Only changes',
-        ServerToClient: 'Only changes',
+        ClientToServer: 'Minimal deltas',
+        ServerToClient: 'Normalized query results, depuded across subscriptions then minimal deltas for changes',
       },
     },
   },
@@ -69,6 +82,22 @@ export const data = LandscapeSchema.make({
     },
     AuthorizationPermissions: {
       data: 'Custom mapping from token attributes to query filters'
+    }
+  },
+  DevelopmentWorkflowsDX: {
+    CLI: {
+      data: 'Yes',
+    },
+    TypeSupport: {
+      data: 'Yes',
+    },
+    DebuggingTools: {
+      data: [
+        'DevTools',
+        'Network Inspector',
+        'Data Inspector',
+        'Dashboard'
+      ],
     }
   }
 })
